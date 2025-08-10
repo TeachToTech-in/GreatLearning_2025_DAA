3eab9eccb846368f00782bd48f8e8dbfd6d818d4
TeachToTech-in <posttoteachtotech@gmail.com>
2025-08-10 12:53:10 +0530

    10-08-2025

---
+++ Rahul_Material/GL_2025 DAA/Code/DAA_2025/src/GreatLearning/Nqueens2.java
@@ -0,0 +1,40 @@
+package GreatLearning;
+
+public class Nqueens2 {
+	public int totalNQueens(int n) {
+        
+
+        
+
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                board[i][j] = '.';
+        int[] leftRow = new int[n];
+        int[] upperDiagonal = new int[2 * n - 1];
+        int[] lowerDiagonal = new int[2 * n - 1];
+        return solve(0, board, leftRow, lowerDiagonal, upperDiagonal);
+    }
+
+    static int solve(int col, char[][] board, int[] leftRow, int[] lowerDiagonal, int[] upperDiagonal) {
+        if (col == board.length) {
+            return 1; // Found one valid solution
+        }
+
+        int count = 0;
+        for (int row = 0; row < board.length; row++) {
+            if (leftRow[row] == 0 && lowerDiagonal[row + col] == 0 && upperDiagonal[board.length - 1 + col - row] == 0) {
+                board[row][col] = 'Q';
+                leftRow[row] = 1;
+                lowerDiagonal[row + col] = 1;
+                upperDiagonal[board.length - 1 + col - row] = 1;
+                count += solve(col + 1, board, leftRow, lowerDiagonal, upperDiagonal);
+                board[row][col] = '.';
+                leftRow[row] = 0;
+                lowerDiagonal[row + col] = 0;
+                upperDiagonal[board.length - 1 + col - row] = 0;
+            }
+        }
+        return count;
+    }
+}
